ee5275fb92dc452aefd88eb00be450c454aa50cc
Ifeanyichukwu Abraham Ossai <abraham.ossai@gmail.com>
2019-06-18 19:00:41 +0100

    update

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -17,7 +17,7 @@ namespace GradeBook
 
     public void AddGrade(double grade)
     {
-      if(grade <= 100 && if grade >= 0)
+      if(grade <= 100 && grade >= 0)
       {
         grades.Add(grade);
       }
